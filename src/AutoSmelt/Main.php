fc0d76029e5aa2c64830aa817ac26ce3a441c508
StrafelessPvP <34737004+StrafelessPvP@users.noreply.github.com>
2017-12-30 15:53:31 -0600

    Update Main.php

--- src/AutoSmelt/Main.php
+++ src/AutoSmelt/Main.php
@@ -22,7 +22,7 @@ use pocketmine\Level;
 class Main extends PluginBase implements Listener{
 
 public function onEnable(){
-$this->getServer()->getLogger()->info("AutoOre Activado!");
+$this->getServer()->getLogger()->info("AutoOre Enabled!");
 $this->getServer()->getPluginManager()->registerEvents($this,$this);
       $this->ores=array(14,15,16,73,56);
  $this->ingot=array(
@@ -41,7 +41,7 @@ $ev->setInstaBreak(true);
 foreach($this->ores as $ore){
 if($block->getId() === $ore && !$ev->isCancelled()){
 $ev->setDrops(array());
-$p->sendPopup("§7-=[ §bEl Ore Esta En Su Inv§7... ]=-");
+$p->sendPopup("§7-=[ §7The Ore Is In Your Inv§7... ]=-");
 $p->getInventory()->addItem(Item::get($this->ingot[$ore]));
                 $x = $p->getX();
                 $y = $p->getY();
